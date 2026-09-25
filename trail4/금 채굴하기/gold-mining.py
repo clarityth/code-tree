0c6a34241f0281620cf089c024163765a993e4ca
TaehyungKim <91403729+clarityth@users.noreply.github.com>
2026-09-26 00:57:06 +0900

    [Passed] 금 채굴하기 (55ms, 17MB)

--- trail4/금 채굴하기/gold-mining.py
+++ trail4/금 채굴하기/gold-mining.py
@@ -2,38 +2,33 @@ n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
-dirs = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+
+from collections import defaultdict
 
 ans = 0
-max_k = 2*(n-1)
-from collections import deque
-
+golds = []
 for row in range(n):
     for col in range(n):
-        visited = set([(row, col)])
-        cnt = [0] * (max_k+1)
+        if grid[row][col] == 1:
+            golds.append((row, col))
 
-        q = deque([(row, col, 0)]) 
-        while q:
-            cur_r, cur_c, cur_step = q.popleft()
+if not golds:
+    print(0)
+    exit(0)
+    
+for row in range(n):
+    for col in range(n):
+        gold_k = defaultdict(int)
+        for (gold_row, gold_col) in golds:
+            k = abs(gold_row-row)+abs(gold_col-col)
+            gold_k[k] += 1
 
-            if grid[cur_r][cur_c] == 1:
-                cnt[cur_step] += 1
-
-            if cur_step == max_k:
-                continue
-
-            for i in range(4):
-                next_r, next_c = cur_r+dirs[i][0], cur_c+dirs[i][1]
-                if 0 <= next_r < n and 0 <= next_c < n:
-                    if (next_r, next_c) not in visited:
-                        visited.add((next_r, next_c))
-                        q.append((next_r, next_c, cur_step+1))
-        
+        max_k = max(gold_k.keys())
         cum_sum = 0
         for k in range(max_k+1):
-            cost = k*k + (k+1)*(k+1)
-            cum_sum += cnt[k]
-            if cost <= cum_sum * m:
+            cost = k*k+(k+1)*(k+1)
+            cum_sum += gold_k[k]
+            if cum_sum*m >= cost:
                 ans = max(ans, cum_sum)
+
 print(ans)
