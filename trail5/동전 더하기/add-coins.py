594ae8be3db825bbf7b3e8c35d4ac7cd08ae13e6
TaehyungKim <91403729+clarityth@users.noreply.github.com>
2026-09-25 15:22:54 +0900

    [Passed] 동전 더하기 (49ms, 16MB)

---
+++ trail5/동전 더하기/add-coins.py
@@ -0,0 +1,11 @@
+n, k = map(int, input().split())
+coins = [int(input()) for _ in range(n)]
+
+# Please write your code here.
+ans = 0
+remain = k
+for i in range(n-1, -1, -1):
+    ans += remain//coins[i]
+    remain = remain - coins[i]*(remain//coins[i])
+
+print(ans)
