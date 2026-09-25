bdfedee848c07bc6e76c8c1dc4744480068e4820
TaehyungKim <91403729+clarityth@users.noreply.github.com>
2026-09-25 21:30:49 +0900

    [Passed] 수 채우기 (47ms, 16MB)

--- trail5/수 채우기/fill-in-number.py
+++ trail5/수 채우기/fill-in-number.py
@@ -8,13 +8,13 @@ if money % 2 == 0:
     print(five+money//2)
     exit(0)
 
-else:
-    while five > 0:
-        money += 5
-        five -= 1
 
-        if money % 2 == 0:
-            print(five+money//2)
-            exit(0)
-    else:
-        print(-1)
+while five > 0:
+    money += 5
+    five -= 1
+
+    if money % 2 == 0:
+        print(five+money//2)
+        exit(0)
+else:
+    print(-1)
